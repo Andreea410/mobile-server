a5e7da50f5aea64e25228d6feedf6664bbc4bdbd
Andreea410 <andreeaungur494@yahoo.com>
2025-12-09 21:42:33 +0200

    Setup

---
+++ src/main/java/com/example/trips_server/TripsServerApplication.java
@@ -0,0 +1,13 @@
+package com.example.trips_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TripsServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TripsServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/trips_server/TripsServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.trips_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TripsServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
